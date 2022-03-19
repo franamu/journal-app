1a49361e2f70bc98971d56d61c70410ecc75fb3e
Francisco Amuchastegui <franamuchastegui@gmail.com>
2022-03-19 13:04:02 -0300

    agrego nuevas screens con diseños

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Sidebar } from './Sidebar';
+import { NoteScreen } from '../notes/NoteScreen';
+// import { NothingSelected } from './NothingSelected';
+
 
 export const JournalScreen = () => {
   return (
-    <div>
-        <h1>
-            JournalScreen
-        </h1>
+    <div className="journal__main-content">  
+      <Sidebar />
+      <main>
+        {/* <NothingSelected /> */}
+        <NoteScreen />
+      </main>
     </div>
   )
 }
